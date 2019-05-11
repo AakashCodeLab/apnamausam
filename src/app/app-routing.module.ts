b1a5dcff5a35245806c89de4ae89928906d07fef
aakash1801 <singh.aakash1801@gmail.com>
2019-05-11 17:21:32 +0530

    add forcast layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'current',
+    pathMatch: 'full'
+  },
+  {
+    path: 'current',
+    loadChildren: './current-weather/current-weather.module#CurrentWeatherModule'
+  },
+  {
+    path: 'forcast',
+    loadChildren: './forcast-weather/forcast-weather.module#ForcastWeatherModule'
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
