1e6bf59850f1af3ff525a300feb1b39841d153ed
aakash1801 <singh.aakash1801@gmail.com>
2019-05-22 09:52:48 +0530

    addedhttps

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+app.use(forceSSL());
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/weather-app'));
 
