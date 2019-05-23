fec649b7c7dea84cc22a5264bf338d232dfabac2
aakash1801 <singh.aakash1801@gmail.com>
2019-05-23 18:54:53 +0530

    added offline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import {MatSnackBar} from '@angular/material';
+import {SwPush, SwUpdate} from '@angular/service-worker';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { ConnectionService } from 'ng-connection-service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  constructor(private spinner: NgxSpinnerService) {
+  status = 'ONLINE';
+  isConnected = true;
+  constructor(private spinner: NgxSpinnerService,private swUpdate: SwUpdate, private swPush: SwPush,
+    private snackbar: MatSnackBar, private connectionService: ConnectionService) {
+
+      this.connectionService.monitor().subscribe(isConnected => {
+        this.isConnected = isConnected;
+        if (this.isConnected) {
+          this.status = "ONLINE";
+        } else {
+          this.status = "OFFLINE";
+        }
+      })
+      this.swUpdate.available.subscribe(evt => {
+  this.swUpdate.checkForUpdate().then(() => {
+  
+          const snack = this.snackbar.open('Update Available', 'Reload',{
+            duration: 7000,
+            panelClass: ['blue-snackbar']
+          });
+          snack
+            .onAction()
+            .subscribe(() => {
+              window.location.reload();
+            });
+  
+          setTimeout(() => {
+            snack.dismiss();
+          }, 5000);
+        });
+      });
+
+
     if (window.navigator) {
       window.navigator.geolocation.getCurrentPosition(position => {
   
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {SwPush, SwUpdate} from '@angular/service-worker';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,9 +10,26 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import {
+
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatSnackBarModule, MatSnackBar
+} from '@angular/material';
+import { OfflineComponent } from './offline/offline.component';
+
 @NgModule({
   declarations: [
     AppComponent,
+    OfflineComponent
   ],
   imports: [
     BrowserModule,
@@ -24,10 +42,21 @@ import { NgxSpinnerModule } from 'ngx-spinner';
      // apiKey: 'AIzaSyDeoGwJDBE4ic-btiuWCfLk37ehqk2LxPs',
       libraries: ['places']
     }),
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSnackBarModule
   ],
   exports: [],
-  providers: [],
+  providers: [SwUpdate],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
